2af45f371f8e70b44f309f46340310b2c8a61ef4
James Sullivan <jsullivan2847@gmail.com>
2022-07-26 15:21:32 -0400

    finished up menu layout

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,6 +4,7 @@ const Menu = () => {
     return (
         <div className="page">
             <img id="menu-logo"src="../images/Logo.png" alt="" />
+            <img id="binoc"src="../images/binoculars.png" alt="" />
             <img id="ny"src="../images/ny.jpg" alt="" />
         </div>
     )
