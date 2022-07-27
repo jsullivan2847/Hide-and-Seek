c10e9a200f32f5ab0e1032565cef456f0981915c
James Sullivan <jsullivan2847@gmail.com>
2022-07-27 16:41:02 -0400

    added some dependencies...trying to wrap my head around all these fetch methods to get google maps on the screen. Would love to write my own but it's so conviluted...

---
+++ src/components/Map.jsx
@@ -0,0 +1,13 @@
+import {useEffect, React} from 'react'
+
+
+export default function Map() {
+
+
+
+  return (
+        <div id='map'>Map</div>
+  )
+}
+
+
--- src/pages/GamePlay.jsx
+++ src/pages/GamePlay.jsx
@@ -1,6 +1,21 @@
+import Map from "../components/Map";
+import { Wrapper, Status} from '@googlemaps/react-wrapper';
 const GamePlay = () => {
+    const key = 'AIzaSyBrL26kHgHcE6O9YC-F7mbxCXhwscpSsdA'
+
+    const render = (status: Status): ReactElement => {
+        if (status === Status.FAILURE) return <h1>failure</h1>;
+        return <h1>loading</h1>;
+      };
+
     return (
-        <h1>GamePlay</h1>
+        <div>
+            <Wrapper apiKey={key} render={render}>
+                <Map/>
+                <Map/>
+            </Wrapper>
+        </div>
+        
     )
 }
 
