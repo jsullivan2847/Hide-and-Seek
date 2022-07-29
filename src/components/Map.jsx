1f22daff19385f1f4cce8e1756d5f9df8e75f038
James Sullivan <jsullivan2847@gmail.com>
2022-07-29 14:16:07 -0400

    map layout matches wireframe

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -3,20 +3,44 @@ import {useEffect, useState, React, useRef} from 'react'
 
 export default function Map() {
 
+  //ref is used to reference the div element the map will be set to...
+  //in this case I'm using it where I would normally use jquery or query selector
   const ref  = useRef(null);
+  const streetRef = useRef(null)
+  //this state will store the entire map object itself
   const [map, setMap] = useState(null);
+  const [streetView, setStreetView] = useState(null)
+
 
   useEffect(() => {
+    //if there is an element to put it into, and there is no map yet 
+    //instantiate a map
     if(ref.current && !map){
       setMap(new window.google.maps.Map(ref.current, {
-        center: {lat: 74.44, lng: 44.949},
+        center: {lat: 42.345573, lng: -71.098326},
         zoom: 8
       }));
+
+      setStreetView(new window.google.maps.StreetViewPanorama(streetRef.current, {
+        position:{lat: 42.345573, lng: -71.098326},
+        pov: {
+          heading: 34,
+          pitch: 10,
+        }
+      }))
     }
+
   }, [ref,map]);
+  //finally if map and ref are truthy - don't do this again. Ref allows the map to persist even on 
+  //re renders 
+
 
   return (
-        <div className='map'style={{width: '500px'}}ref={ref}>Map</div>
+    <div className='maps-container'>
+      <div className='streetview'ref={streetRef}></div>
+       <div className='map'ref={ref}></div>
+    </div>
+
   )
 }
 
--- src/pages/GamePlay.jsx
+++ src/pages/GamePlay.jsx
@@ -3,17 +3,16 @@ import { Wrapper, Status} from '@googlemaps/react-wrapper';
 const GamePlay = () => {
     const key = 'AIzaSyBrL26kHgHcE6O9YC-F7mbxCXhwscpSsdA'
 
+    //This function 
     const render = (status: Status): ReactElement => {
         if (status === Status.FAILURE) return <h1>failure</h1>;
         return <h1>loading</h1>;
       };
 
-    console.log(Status.FAILURE)
 
     return (
         <div>
             <Wrapper apiKey={key} render={render}>
-                <Map/>
                 <Map/>
             </Wrapper>
         </div>
