ac7e52031ae096fcee7c5f150ca7d1df00188a7f
James Sullivan <jsullivan2847@gmail.com>
2022-08-26 14:48:54 -0400

    links work

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../Footer/Footer.css'
 
 export default function Footer() {
@@ -8,9 +9,15 @@ export default function Footer() {
         <p>Developed by James Sullivan © 2022</p>
         </div>
         <div className='icon-container'>
-        <img className='icon' src="../images/twitter.png"  alt="" />
-        <img className='icon' src="../images/github.png" alt="" />
-        <img className='icon' src="../images/linkedIn.png" alt="" />
+            <a href='https://twitter.com/popcornfamine'>
+            <img className='icon' src="../images/twitter.png"  alt="twitter" />
+            </a>
+            <a href="https://github.com/jsullivan2847">
+            <img className='icon' src="../images/github.png" alt="github" />
+            </a>
+            <a href="https://www.linkedin.com/in/james-d-sullivan/">
+            <img className='icon' src="../images/linkedIn.png" alt="linkedIn" />
+            </a>
         </div>
     </div>
   )
